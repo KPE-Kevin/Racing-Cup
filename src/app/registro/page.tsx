7c3a3d03bf24523fc0d0b24f20fae0415513fc8a
Claudia Michelle Utrilla Flores <78976069+michelle0228@users.noreply.github.com>
2024-03-19 17:09:54 -0600

    Add files via upload

---
+++ src/app/registro/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+import Image from "next/image";
+import React from "react";
+
+
+const formPage = () => (
+  <div>
+    <div>
+      <h1 id='titulo'><strong>Registro</strong></h1>
+      </div>
+      <div id='esp'>
+      </div>
+      <div id='form'>
+     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeWrNQuoBQGk-DJAYZRro_Xew7mNBlXMtgs92upOtzNwI5_JQ/viewform?embedded=true" 
+     width="640" 
+     height="1428" 
+    
+  >Cargando…</iframe>
+  </div>
+  </div>
+);
+
+export default formPage;
